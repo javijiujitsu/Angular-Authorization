6fc7ccf3014de81c80f1f47dff77df4650b6f099
Javier Buitrago <jbuitrago2014@fau.edu>
2017-11-18 17:06:19 -0500

    hide the bcript password

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -35,6 +35,7 @@ router.post('/signup', (req, res, next) => {
           return;
         }
         req.login(theUser, (err) => {
+          theUser.password = undefined;
         res.status(200).json(req.user);
       });
     });
@@ -63,6 +64,7 @@ router.post('/login', (req, res, next) => {
   }
 
    req.login(foundUser, (err) => {
+     foundUser.password = undefined;
       res.status(200).json(foundUser);
    });
 
