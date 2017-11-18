bb37664fcaf80015bfed8dc9ec3036e152ed6869
Javier Buitrago <jbuitrago2014@fau.edu>
2017-11-18 15:40:22 -0500

    added session to app.js

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 const  mongoose = require('mongoose');
+const passport   = require('passport');
+const session    = require('express-session');
+
+
 
 
 var index = require('./routes/index');
@@ -28,9 +32,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'string is to avoid a warning',
+  resave: true,
+  saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Routes
 
 app.use('/', index);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
